d61ae9a9e97935d0f62fc6f088bc1fa488f5b55b
Armin Wasicek <armin.wasicek@avast.com>
2018-11-30 08:01:34 -0800

    added data dir to run script

--- run_docker.sh
+++ run_docker.sh
@@ -3,6 +3,7 @@
 DOCKERIMAGE=hellospencer/jupyterspark23
 JUPYTERPORT=8899
 NOTEBOOKDIR=/Users/armin.wasicek/Source/notebooks
+DATADIR=/Users/armin.wasicek/Data
 
 if [[ $* == *-d* ]]
 then
@@ -11,5 +12,5 @@ else
     DAEMONIZE=""
 fi
 
-docker run $DAEMONIZE -p $JUPYTERPORT:8888 -p 8080:8080 -p 7077:7077 -v $NOTEBOOKDIR:/home/jupyter/notebooks $DOCKERIMAGE
+docker run $DAEMONIZE -p $JUPYTERPORT:8888 -p 8080:8080 -p 7077:7077 -v $NOTEBOOKDIR:/home/jupyter/notebooks -v $DATADIR:/home/jupyter/notebooks/Data $DOCKERIMAGE
 
